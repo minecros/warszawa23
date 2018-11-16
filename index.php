127b632e5deb5fe1e3b71a4c9272a42781edd7a4
Daniel Ross <dr68447@gmail.com>
2018-11-16 20:08:40 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet"> 
+        <title>Warszawa23.pl</title>
+    </head>
+    <body>
+        <header>
+            <!-- Tu zrobić logo  -->
+           
+            
+            <nav>
+                
+                
+                <ul class="menu">
+                    <li><a href="index.php"> 	&#xf64f;WARSZAWA23</a></li>
+                    <li><a href="index.php">FIRMY</a></li>
+                    <li><a href="index.php">ANONSE</a></li>
+                    <li><a href="index.php">STRONY</a></li>
+                    <li><a href="index.php">FILMY</a></li>
+                    <li><a href="index.php">GALERIE</a></li>
+                    <li><a href="index.php">FORUM</a></li>
+                </ul>
+            </nav>
+        </header>
+    
+    </body>
+    
+</html>
